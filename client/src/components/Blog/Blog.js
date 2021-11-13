dad3c02b639fe006517dbe6128686a9df2b512bc
Aparna UdayKumar <aparnaudaykumar@Aparnas-MacBook-Air.local>
2021-11-13 13:38:18 +0530

    fix: issue 3

--- client/src/components/Blog/Blog.js
+++ client/src/components/Blog/Blog.js
@@ -28,13 +28,15 @@ function Blog (){
             </img>
             <a href='https://aparna-u.hashnode.dev/simple-method-to-convert-text-into-speech-using-python'>Simple method to convert text into speech using python</a>
         </div> */}
-        <div class=" card card-center" style={{width: '40rem'}}>
+        <div className='card-res'>
+        <div className="card-center width" style={{width: '40rem'}}>
             <img className="card-img-top" onClick={handleClick} src="https://aparna-u.hashnode.dev/_next/image?url=https%3A%2F%2Fcdn.hashnode.com%2Fres%2Fhashnode%2Fimage%2Fupload%2Fv1601996797751%2Fj9iAErmR1.png%3Fw%3D1600%26h%3D840%26fit%3Dcrop%26crop%3Dentropy%26auto%3Dcompress%2Cformat%26format%3Dwebp&w=3840&q=75" alt="Card image cap"/>
             <div className="card-body color-grey">
                 <h5 className="card-title font-24 "> <a style={{"color":`${bgcolor}`, "text-decoration":'none', }}onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave} href="https://aparna-u.hashnode.dev/simple-method-to-convert-text-into-speech-using-python" className="text-center" >Simple method to convert text into speech using python</a></h5>
-                <p class="card-text">Google Text to Speech API known as the gTTS API. It converts text entered by the user, into audio which can be saved as a mp3 file.</p>
-                <a href="https://aparna-u.hashnode.dev/simple-method-to-convert-text-into-speech-using-python" class="btn btn-secondary">Read more</a>
+                <p className="card-text">Google Text to Speech API known as the gTTS API. It converts text entered by the user, into audio which can be saved as a mp3 file.</p>
+                <a href="https://aparna-u.hashnode.dev/simple-method-to-convert-text-into-speech-using-python" className="btn btn-secondary">Read more</a>
             </div>
+        </div>
         </div>
         </div>
     )
--- client/src/components/Education/Education.js
+++ client/src/components/Education/Education.js
@@ -1,10 +1,59 @@
+/* eslint-disable jsx-a11y/alt-text */
 import React from 'react';
 import './Education.css'
+import img from './education-fill.svg'
 function Education (){
     return (
-        <div className="div3">
-        <h2 style={{"font-family": 'Crete Round, serif', 'font-size': '48px'}}>Education</h2>
+        <div className="div3 pt-5">
+            <h2 style={{"font-family": 'Crete Round, serif', 'font-size': '48px', 'text-align':'center'}}>
+                Education
+            </h2>
+            <div className='container pt-5'>
+                <div className='row no-gutters justify-content-center flex-wrap' >
+                    <div className='col icon'>
+                    <img
+                            src={img}
+                            height='35px'
+
+                        >
+                    </img>
+                    </div>
+                    <div className='col description'>
+                        <p> Vimal Jyothi Engineering College</p>
+                        <p>B.Tech CSE, 2020</p>
+                    </div>
+                </div>
+                <div className='row no-gutters justify-content-center flex-wrap pt-5' >
+                    <div className='col icon'>
+                    <img
+                            src={img}
+                            height='35px'
+
+                        >
+                    </img>
+                    </div>
+                    <div className='col description'>
+                        <p> Kendriya Vidyalaya Keltron Nagar</p>
+                        <p>12th,2016</p>
+                    </div>
+                </div>
+                <div className='row no-gutters justify-content-center flex-wrap pt-5' >
+                    <div className='col icon'>
+                    <img
+                            src={img}
+                            height='35px'
+
+                        >
+                    </img>
+                    </div>
+                    <div className='col description '>
+                        <p> Kendriya Vidyalaya Keltron Nagar</p>
+                        <p>10th,2014</p>
+                    </div>
+                </div>
+            </div>
         </div>
+      
     )
 
 }
